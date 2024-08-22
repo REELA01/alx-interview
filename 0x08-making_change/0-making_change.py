43ca0f603feb39591b2670199d3542cb18e2257f
reela <sudanrela123@gmail.com>
2024-08-22 01:31:36 +0000

    tst

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""change making module"""
+
+
+def makeChange(coins, total):
+    """cetermines the fewest number of coins needed"""
+    if total <= 0:
+        return 0
+    remi = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while remi > 0:
+        if coin_idx >= n:
+            return -1
+        if remi - sorted_coins[coin_idx] >= 0:
+            remi -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
